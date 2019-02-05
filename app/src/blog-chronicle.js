048ae80268e2b2d8db298282ebe2dff8cf6ecaf7
Justin Ribeiro <justin@justinribeiro.com>
2019-02-04 17:50:05 -0800

    Remove iron-list, causing problems of late.

--- app/src/blog-chronicle.js
+++ app/src/blog-chronicle.js
@@ -1,5 +1,4 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-list/iron-list.js';
 import {BlogUtils} from './blog-utils-mixin.js';
 import './blog-network-warning.js';
 import './shared-styles.js';
@@ -33,12 +32,6 @@ class BlogChronicle extends BlogUtils(PolymerElement) {
       url: '/data/chronicle/index.json',
       onLoad: (e) => {
         this.set('blog', JSON.parse(e.target.responseText));
-
-        // failsafe
-        setTimeout(() => {
-          this.shadowRoot.querySelector('iron-list').notifyResize();
-        }, 2000);
-
         this.set('failure', false);
       },
       onError: (e) => {
@@ -50,42 +43,13 @@ class BlogChronicle extends BlogUtils(PolymerElement) {
   static get template() {
     return html`
       <style include="shared-styles">
-        #main {
-          margin: initial;
-          max-width: initial;
-          padding-right: 0;
-          padding-left: 0;
-          display: block;
-          width: 100%;
+        #posts > h2 {
+          margin-bottom: 10px;
         }
 
-        #main > div {
-          margin: auto;
-          padding: 0 20px;
-          max-width: 800px;
-        }
-
-        #main > iron-list {
-          margin: auto;
-          padding: 0 20px;
-          width: 800px;
-        }
-
-        iron-list {
-          flex: 1 1 auto;
-        }
-
-        @media (max-width: 767px) {
-          #main {
-            position: absolute;
-            padding-top: var(--header-height-mobile);
-            height: 100vh;
-            overflow-y: scroll;
-          }
-          #main > iron-list {
-            width: initial;
-            margin: initial;
-          }
+        #posts {
+          display: flex;
+          flex-wrap: wrap;
         }
       </style>
 
@@ -96,8 +60,8 @@ class BlogChronicle extends BlogUtils(PolymerElement) {
           <blockquote>a usually continuous historical account of events arranged in order of time without analysis or interpretation</blockquote>
           <p>My chronicles aren’t exactly without analysis, but they are listing of events and happenings. The latest events are listed on the home page and the archives are presented below.</p>
         </div>
-        <iron-list scroll-target="main" items="[[blog.posts]]" as="post" grid>
-          <template>
+        <div id="posts">
+          <template is="dom-repeat" items="[[blog.posts]]" as="post">
             <div class="post-container">
               <a href="[[post.permalink]]">
                 <h3 class="date">🗒️ [[post.date]]</h3>
@@ -105,7 +69,7 @@ class BlogChronicle extends BlogUtils(PolymerElement) {
               </a>
             </div>
           </template>
-        </iron-list>
+        </div>
       </div>
 
       <blog-network-warning hidden$="[[!failure]]"></blog-network-warning>
