f8fb23973908a6e186148189a522fc8908156440
Justin Ribeiro <justin@justinribeiro.com>
2019-04-08 13:52:26 -0700

    remove icon deps

--- app/src/blog-missing.js
+++ app/src/blog-missing.js
@@ -1,14 +1,7 @@
 import {LitElement, html} from '@polymer/lit-element';
-import '@polymer/iron-icon/iron-icon.js';
-import './blog-icons.js';
 
 class BlogMissing extends LitElement {
-
-  constructor() {
-    super();
-  }
-
-  _render() {
+  render() {
     return html`
       <style>
         :host {
@@ -23,17 +16,17 @@ class BlogMissing extends LitElement {
           padding-top: 135px;
         }
 
-        iron-icon {
+        svg {
           display: inline-block;
           width: 150px;
           height: 150px;
         }
       </style>
-
-      <!-- So, since we don't load everything -->
       <div id="main">
         <div>
-          <iron-icon icon="error"></iron-icon>
+          <svg viewbox="0 0 24 24">
+            <g id="no-internet"><path fill-opacity=".3" d="M24.24 8l1.35-1.68C25.1 5.96 20.26 2 13 2S.9 5.96.42 6.32l12.57 15.66.01.02.01-.01L20 13.28V8h4.24z"></path><path d="M22 22h2v-2h-2v2zm0-12v8h2v-8h-2z"></path></g>
+          </svg>
           <h1>Sorry, I couldn't find that page.</h1>
         </div>
         <a href="/">Go to the home page</a>
