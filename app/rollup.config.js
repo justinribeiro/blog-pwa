f227e23093a7a7e0e19d119b04b618cbd0825412
Justin Ribeiro <justin@justinribeiro.com>
2019-04-10 11:21:05 -0700

    move to rollup config for pwa builds

---
+++ app/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from 'rollup-plugin-node-resolve';
+import minify from 'rollup-plugin-babel-minify';
+
+export default {
+  input: [
+    'src/app.js',
+    'src/blog-pwa.js',
+    'src/blog-static.js',
+    'src/blog-entry.js',
+    'src/blog-chronicle.js',
+    'src/lazy-resources.js',
+    'src/3d-utils.js',
+    'src/code-block.js'
+  ],
+  output: {
+    dir: 'build/default/src',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    minify({
+      mangle: {
+        exclude: { "__import": true }
+      },
+      comments: false,
+      plugins: ['transform-remove-console']
+    })
+  ]
+};
